cb2aac313ad32855caac1f23d6ba23ec3751b8a9
Sergey Korshikov <seregakor@mail.ru>
2021-06-03 22:36:51 +0300

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
 	mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
 	devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	],
 };
